08ebfe58d2801f739065d8839c968a459b4d2720
Walter Sangroni <walterhugosangroni@gmail.com>
2023-11-08 08:34:19 -0300

    Avance en listado producto, falta resolver que se muestre el producto agregado

--- web/src/components/ListadoProductos.jsx
+++ web/src/components/ListadoProductos.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Button, Modal, Form } from "react-bootstrap";
 
 const initialProducts = [
   {
@@ -6,19 +7,63 @@ const initialProducts = [
     articulo: "1",
     talles: ["s", "m", "xl", "xxl"],
     colores: ["blanco", "amarillo"],
+    datosPorTalleYColor: {
+      s: {
+        blanco: 5,
+        amarillo: 8,
+      },
+      m: {
+        blanco: 1,
+        amarillo: 0,
+      },
+      xl: {
+        blanco: 0,
+        amarillo: 3,
+      },
+      xxl: {
+        blanco: 0,
+        amarillo: 0,
+      },
+    },
   },
   {
     id: 2,
     articulo: "2",
     talles: ["s", "m"],
     colores: ["blanco", "amarillo", "negro", "fuccia"],
+    datosPorTalleYColor: {
+      s: {
+        blanco: 1,
+        amarillo: 8,
+        negro: 5,
+        fuccia: 0,
+      },
+      m: {
+        blanco: 1,
+        amarillo: 8,
+        negro: 5,
+        fuccia: 0,
+      },
+    },
   },
   {
     id: 3,
     articulo: "3",
     talles: ["s", "m", "xl"],
     colores: ["blanco"],
+    datosPorTalleYColor: {
+      s: {
+        blanco: 1,
+      },
+      m: {
+        blanco: 5,
+      },
+      xl: {
+        blanco: 4,
+      },
+    },
   },
+
   {
     id: 4,
     articulo: "4",
@@ -46,39 +91,89 @@ const initialProducts = [
 ];
 
 const ListadoProductos = () => {
-  const [products] = useState(initialProducts);
+  const [products, setProducts] = useState(initialProducts);
   const [selectedProduct, setSelectedProduct] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const [newProduct, setNewProduct] = useState({
+    articulo: "",
+    talle: "",
+    color: "",
+  });
 
   const handleProductClick = (product) => {
     setSelectedProduct(product);
   };
 
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleNewProductChange = (event) => {
+    const { name, value } = event.target;
+    setNewProduct({
+      ...newProduct,
+      [name]: value,
+    });
+  };
+
+  const handleSaveProduct = () => {
+    // Crea un nuevo objeto con los datos del nuevo producto
+    const newProductData = {
+      id: products.length + 1, // Asigna un nuevo ID
+      ...newProduct,
+    };
+
+    // Agrega el nuevo producto a la lista de productos
+    setProducts([...products, newProductData]);
+
+    // Cierra el modal
+    setIsModalOpen(false);
+  };
+
   const renderGrilla = (product) => {
-    if (product.talles && product.colores) {
-      return (
-        <table className="table-grilla">
-          <thead>
-            <tr className="table-header-grilla">
-              <th className="articulo-grilla">{product.articulo}</th>
-              {product.talles.map((talle, index) => (
-                <th key={index}>{talle}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {product.colores.map((color, index) => (
-              <tr key={index}>
-                <td className="table-cell-grilla">{color}</td>
-                {product.talles.map((talle, talleIndex) => (
-                  <td key={talleIndex}>
-                    {/* Aquí puedes mostrar datos específicos para cada combinación de talle y color */}
-                  </td>
+    if (product && product.talles && product.colores) {
+      if (product.datosPorTalleYColor) {
+        return (
+          <table className="table-grilla">
+            <thead>
+              <tr className="table-header-grilla">
+                <th className="articulo-grilla">{product.articulo}</th>
+                {product.talles.map((talle, index) => (
+                  <th key={index}>Talle {talle}</th>
                 ))}
               </tr>
-            ))}
-          </tbody>
-        </table>
-      );
+            </thead>
+            <tbody>
+              {product.colores.map((color, index) => (
+                <tr key={index}>
+                  <td className="table-cell-grilla">{color}</td>
+                  {product.talles.map((talle, talleIndex) => (
+                    <td key={talleIndex}>
+                      {/* Aquí puedes mostrar datos específicos para cada combinación de talle y color */}
+                      {selectedProduct.datosPorTalleYColor &&
+                        selectedProduct.datosPorTalleYColor[talle] &&
+                        selectedProduct.datosPorTalleYColor[talle][color]}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        );
+      } else {
+        return (
+          <div className="grilla-NoDatos">
+            No hay datos de talle y color disponibles.
+          </div>
+        );
+      }
+    } else {
+      return <div className="grilla-NoDatos">Producto no válido.</div>;
     }
   };
 
@@ -99,8 +194,70 @@ const ListadoProductos = () => {
             ))}
           </tbody>
         </table>
+        <button onClick={openModal} className="agregar-producto-grilla">
+          Agregar Producto
+        </button>
         {selectedProduct && renderGrilla(selectedProduct)}
       </div>
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <Button variant="primary" onClick={openModal}></Button>
+            <Modal
+              show={isModalOpen}
+              onHide={closeModal}
+              backdrop="static"
+              keyboard={false}
+            >
+              <Modal.Header closeButton>
+                <Modal.Title>Nuevo producto</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Form>
+                  <Form.Group controlId="articulo">
+                    <Form.Label>Número de Artículo</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="articulo"
+                      value={newProduct.articulo}
+                      onChange={handleNewProductChange}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="talle">
+                    <Form.Label>Talle</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="talle"
+                      value={newProduct.talle}
+                      onChange={handleNewProductChange}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="color">
+                    <Form.Label>Color</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="color"
+                      value={newProduct.color}
+                      onChange={handleNewProductChange}
+                    />
+                  </Form.Group>
+                </Form>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button id="botonNuevoCliente" onClick={closeModal}>
+                  Cerrar
+                </Button>
+                <Button id="botonNuevoCliente" onClick={handleSaveProduct}>
+                  Guardar
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </div>
+        </div>
+      )}
     </>
   );
 };
