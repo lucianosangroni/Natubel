09c377e17270c7276eb33e3aa6eb0c36932346ce
Luciano Sangroni <lsangroni@frba.utn.edu.ar>
2023-12-17 22:45:43 -0300

    prueba

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -5,24 +5,25 @@ const jwt = require("jsonwebtoken");
 
 const login = async (req, res) => {
     try {
-      req = matchedData(req);
+      //req = matchedData(req);
 
-      const admin = await adminModel.findOne({ where: { nombre_usuario: req.nombre_usuario } });
-      if (!admin) {
-        return res.status(404).json({ message: 'Usuario no encontrado' });
-      }
+      //const admin = await adminModel.findOne({ where: { nombre_usuario: req.nombre_usuario } });
+      //if (!admin) {
+      //  return res.status(404).json({ message: 'Usuario no encontrado' });
+      //}
+//
+      //const checkPassword = await bcrypt.compare(req.password, admin.password);
+//
+      //if (!checkPassword) {
+      //  return res.status(403).json({ message: 'Contraseña incorrecta' });
+      //}
+//
+      //const tokenJwt = jwt.sign({ username: admin.nombre_usuario }, process.env.JWT_SECRET,{ expiresIn: "10h" })
 
-      const checkPassword = await bcrypt.compare(req.password, admin.password);
-
-      if (!checkPassword) {
-        return res.status(403).json({ message: 'Contraseña incorrecta' });
-      }
-
-      const tokenJwt = jwt.sign({ username: admin.nombre_usuario }, process.env.JWT_SECRET,{ expiresIn: "10h" })
-
-      res.status(201).json({ message: 'Inicio de sesion exitoso', jwt: tokenJwt });
+      //res.status(201).json({ message: 'Inicio de sesion exitoso', jwt: tokenJwt });
+      res.status(200).json({req})
     } catch (e) {
-        res.status(500).json({ message: 'Error al iniciar sesion: ', e });
+        res.status(500).json({ message: 'Error al iniciar sesion' });
     }
   };
 
