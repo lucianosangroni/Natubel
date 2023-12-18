5bd522437ff3ef9053ce8b5c1fa3323be798bfd0
Luciano Sangroni <lsangroni@frba.utn.edu.ar>
2023-12-17 22:48:57 -0300

    console.log de prueba

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -23,6 +23,7 @@ const login = async (req, res) => {
       //res.status(201).json({ message: 'Inicio de sesion exitoso', jwt: tokenJwt });
       res.status(200).json({req})
     } catch (e) {
+      console.error(e)
         res.status(500).json({ message: 'Error al iniciar sesion' });
     }
   };
