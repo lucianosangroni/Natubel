770f6a0c4849dffd1fecbe04485956e2fd3c5ffe
LucianoSangroni123 <lsangroni@frba.utn.edu.ar>
2024-02-07 09:30:04 -0300

    categorias e imagenes

--- tienda/src/components/item/Item.jsx
+++ tienda/src/components/item/Item.jsx
@@ -4,16 +4,20 @@ import { Link } from 'react-router-dom';
 import { toCapital } from '../../helpers/toCapital';
 
 const Item = ( { producto } ) => {
-  
+  console.log(producto)
   return (
     <div className="producto">
-      {/*<img src={producto.images} alt={producto.numero_articulo} />*/}
+      {producto.imagens.length > 0 ? (
+        <img src={producto.imagens[0].url} alt={"sin imagen"} />
+      ) : (
+        <img src={"http://localhost:3001/no-hay-foto.png"} alt={"sin imagen"} />
+      )}
       <div className='productoInicio'>
             <h4>{producto.numero_articulo}</h4>
             <p>Precio: ${producto.precio_minorista}</p>
-            {/*{producto.categorias.map((categoria, index) => (
-              <p key={index}>Categoria: {toCapital(categoria)}</p>
-            ))}*/}
+            {producto.categoria.map((cat, index) => (
+              <p key={index}>Categoria: {toCapital(cat.nombre)}</p>
+            ))}
             <Link className='ver-mas' to={`/item/${producto.id}`}>Detalle</Link>
       </div>
     </div>
