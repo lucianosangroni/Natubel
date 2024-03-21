3509fecd5e6eac27b741680f1ffd422855e0a119
Luciano Sangroni <lsangroni@frba.utn.edu.ar>
2024-03-21 07:13:00 -0300

    borro api y tienda

--- api/rutas/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { login } = require("../controllers/auth");
-const { validateLogin } = require("../validators/auth");
-
-router.post("/", validateLogin, login);
-
-module.exports = router;
