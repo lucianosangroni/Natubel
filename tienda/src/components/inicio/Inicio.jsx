39e95f18dbe7b14c9333b33815730a0bfe173c19
Luciano Sangroni <lsangroni@frba.utn.edu.ar>
2024-07-16 10:04:03 -0300

    slide envios al pais

--- tienda/src/components/inicio/Inicio.jsx
+++ tienda/src/components/inicio/Inicio.jsx
@@ -17,51 +17,81 @@ const Inicio = () => {
       <div style={{ marginTop: "1rem" }}>
         <UncontrolledExample />
         <div className="enviamosPagaSeguridad">
-  <div className="slide-container">
-    <div className="a">
-      <img src={truck} alt="envios" />
-      <div>
-        <h3>Enviamos tu compra</h3>
-        <p>Entregas a todo el país</p>
-      </div>
-    </div>
-    <div className="a">
-      <img src={creditCard} alt="envios" />
-      <div>
-        <h3>Paga como quieras</h3>
-        <p>Tarjeta débito/crédito o efectivo</p>
-      </div>
-    </div>
-    <div className="a">
-      <img src={fileLock} alt="envios" />
-      <div>
-        <h3>Compra con seguridad</h3>
-        <p>Tus datos siempre protegidos</p>
-      </div>
-    </div>
-    <div className="a">
-      <img src={truck} alt="envios" />
-      <div>
-        <h3>Enviamos tu compra</h3>
-        <p>Entregas a todo el país</p>
-      </div>
-    </div>
-    <div className="a">
-      <img src={creditCard} alt="envios" />
-      <div>
-        <h3>Paga como quieras</h3>
-        <p>Tarjeta débito/crédito o efectivo</p>
-      </div>
-    </div>
-    <div className="a">
-      <img src={fileLock} alt="envios" />
-      <div>
-        <h3>Compra con seguridad</h3>
-        <p>Tus datos siempre protegidos</p>
-      </div>
-    </div>
-  </div>
-</div>
+          <div className="slide-container">
+            <div className="a">
+              <img src={truck} alt="envios" />
+              <div>
+                <h3>Enviamos tu compra</h3>
+                <p>Entregas a todo el país</p>
+              </div>
+              <img className="imgCelSlide" src={truck} alt="envios" />
+            </div>
+            <div className="a">
+              <img src={truck} alt="envios" />
+              <div>
+                <h3>Enviamos tu compra</h3>
+                <p>Entregas a todo el país</p>
+              </div>
+              <img className="imgCelSlide" src={truck} alt="envios" />
+            </div>
+            <div className="a">
+              <img src={truck} alt="envios" />
+              <div>
+                <h3>Enviamos tu compra</h3>
+                <p>Entregas a todo el país</p>
+              </div>
+              <img className="imgCelSlide" src={truck} alt="envios" />
+            </div>
+            <div className="a">
+              <img src={truck} alt="envios" />
+              <div>
+                <h3>Enviamos tu compra</h3>
+                <p>Entregas a todo el país</p>
+              </div>
+              <img className="imgCelSlide" src={truck} alt="envios" />
+            </div>
+            <div className="a">
+              <img src={truck} alt="envios" />
+              <div>
+                <h3>Enviamos tu compra</h3>
+                <p>Entregas a todo el país</p>
+              </div>
+              <img className="imgCelSlide" src={truck} alt="envios" />
+            </div>
+            <div className="a">
+              <img src={truck} alt="envios" />
+              <div>
+                <h3>Enviamos tu compra</h3>
+                <p>Entregas a todo el país</p>
+              </div>
+              <img className="imgCelSlide" src={truck} alt="envios" />
+            </div>
+            <div className="a">
+              <img src={truck} alt="envios" />
+              <div>
+                <h3>Enviamos tu compra</h3>
+                <p>Entregas a todo el país</p>
+              </div>
+              <img className="imgCelSlide" src={truck} alt="envios" />
+            </div>
+            <div className="a">
+              <img src={truck} alt="envios" />
+              <div>
+                <h3>Enviamos tu compra</h3>
+                <p>Entregas a todo el país</p>
+              </div>
+              <img className="imgCelSlide" src={truck} alt="envios" />
+            </div>
+            <div className="a">
+              <img src={truck} alt="envios" />
+              <div>
+                <h3>Enviamos tu compra</h3>
+                <p>Entregas a todo el país</p>
+              </div>
+              <img className="imgCelSlide" src={truck} alt="envios" />
+            </div>
+          </div>
+        </div>
         <ItemListContainer flagCatalogo={false} /> 
       </div>
     </>
