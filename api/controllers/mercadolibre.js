fd0700b4aa91592ab9d0be8d25a1199167d92fd0
Luciano Sangroni <lsangroni@frba.utn.edu.ar>
2026-04-16 11:38:48 -0300

    prueba publicar en ml con menos atributos

--- api/controllers/mercadolibre.js
+++ api/controllers/mercadolibre.js
@@ -93,16 +93,9 @@ const createItem = async (req, res) => {
 
                 attributes: [
                     { id: "BRAND", value_name: articulo.marca.nombre },
-                    //{ id: "MODEL", value_name: "Air Zoom " + articulo.numero_articulo },
-                    //{ id: "GENDER", value_name: "Mujer" },
                     { id: "COLOR", value_name: p.color },
                     { id: "SIZE", value_name: p.talle },
                     { id: "EMPTY_GTIN_REASON", value_name: "El producto no tiene código registrado" },
-                    //{ id: "IS_KIT", value_name: "No" },
-                    //{ id: 'SELLER_PACKAGE_HEIGHT', value_name: '5 cm' },
-                    //{ id: 'SELLER_PACKAGE_WIDTH',  value_name: '30 cm' },
-                    //{ id: 'SELLER_PACKAGE_LENGTH', value_name: '40 cm' },
-                    //{ id: 'SELLER_PACKAGE_WEIGHT', value_name: '500 g' }
                 ],
             };
 
